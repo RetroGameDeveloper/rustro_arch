047c3ca6f2d00a9910317ce27e8df86ecc0d7cbd
A <>
2023-03-27 12:57:35 +0100

    Added really simple miniFB window that has a blue pixel that you can move around to create lines on the screen

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 480;
+
+fn main() {
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut window = Window::new(
+        "Rust Game",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    ).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600))); // ~60fps
+
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        // Move the pixel when the arrow keys are pressed
+        if window.is_key_down(Key::Left) && x > 0 {
+            x -= 1;
+        }
+        if window.is_key_down(Key::Right) && x < WIDTH - 1 {
+            x += 1;
+        }
+        if window.is_key_down(Key::Up) && y > 0 {
+            y -= 1;
+        }
+        if window.is_key_down(Key::Down) && y < HEIGHT - 1 {
+            y += 1;
+        }
+
+        // Set the pixel to blue
+        buffer[y * WIDTH + x] = 0x0000FFFF;
+
+        // Update the window buffer and display the changes
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
+}
